5437888ba4e2ba00440617f6f4204e4173be3544
bacho1927 <bacho.karashvili@gmail.com>
2025-09-21 15:49:20 +0400

    fixed style erros on main and products detail page

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -2,6 +2,9 @@ import { useParams } from 'react-router-dom';
 import { FetchProductById } from '../features/auth/fetch/FetchProductById';
 import { useState,useEffect } from 'react';
 import './ProductDetail.css'
+import { MdOutlineShoppingCart } from "react-icons/md";
+
+
 function ProductDetail() {
 
     const { productId } = useParams(); 
@@ -10,8 +13,10 @@ function ProductDetail() {
     const [error, setError] = useState(null);
     const [selectedColor, setSelectedColor] = useState(product?.available_colors?.[0] || '');
     const [selectedSize, setSelectedSize] = useState('');
+    const [mainImage, setMainImage] = useState(product?.cover_image);
+    const [quantity, setQuantity] = useState(1);
 
-
+ // this code fetches products by id
    useEffect(() => {
     const loadProduct = async () => {
       try {
@@ -35,24 +40,45 @@ function ProductDetail() {
     }
     }, [product]);
         
+
     useEffect(() => {
   if (product?.available_sizes?.length) {
     setSelectedSize(product.available_sizes[0]);
   }
 }, [product]);
+
+// this code is to display main image on initial render
+  useEffect(() => {
+    if (product?.cover_image) {
+      setMainImage(product.cover_image);
+    }
+  }, [product]);
+
+
 console.log(product)
+
     return (
         <div className="Product-Detail-Container">
-            
-            <img class='Product-Detail-Main-Image'  src={product?.cover_image} alt={product?.name}  />
+            <div class="Product-Detail-Main-Image-Container">
+              <div class="Product-Small-Images-Container">
+                {product?.images?.map((image,index) =>(
+                  <img src={image} 
+                  key={index}
+                   onClick={() => setMainImage(image)}class="Product-Small-Images"
+                   style={{border: mainImage === image ? `1px solid gray` : ''}}/>
+                ))}
+                </div>
+              <img class='Product-Detail-Main-Image'  src={mainImage} alt={product?.name}  />
+              
+            </div>
             <div className='Product-Description-Info-Container'>
             <h1>{product?.name}</h1>
             <div className="Product-Detail-Info">
                 <h1 className="Product-Detail-Price">${product?.price}</h1> 
-                {/* <p className="Product-Detail-Description">{product?.description}</p> */}
+                
                 
 
-                <p>{`Color: ${selectedColor}`}</p>
+                <p class="Product-Color-Text">{`Color: ${selectedColor}`}</p>
 
                 <div className='Product-Colors-Container'>
 
@@ -84,7 +110,28 @@ console.log(product)
                         {size}
                     </div>
                 ))}
-            </div>            
+                </div>            
+             </div>
+             <div className="Quantity-Selector">
+              <label htmlFor="quantity">Quantity </label>
+                  <select
+                    id="quantity"
+                    value={quantity}
+                    onChange={(e) => setQuantity(Number(e.target.value))}
+                  >
+                    {Array.from({ length: 10 }, (_, i) => (
+                      <option key={i + 1} value={i + 1}>
+                        {i + 1}
+                      </option>
+                    ))}
+                  </select>
+            </div>
+             <button class="Add-To-Cart-Button"><MdOutlineShoppingCart />Add to cart</button>
+             <div class="Product-Brand-Details-Container">
+              <p class='Details-Text'>Details</p>
+
+              <p >Brand:  {product?.brand?.name}</p>
+              <p>{product?.description}</p>
              </div>
              </div>
         </div>
